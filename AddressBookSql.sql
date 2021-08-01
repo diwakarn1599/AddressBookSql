92c2a0573f278b6bb016ee115cede23e074a12a6
Diwakar <diwakar.n@tvsnext.io>
2021-08-01 21:06:20 +0530

    [Diwakar] Create address book Table

--- AddressBookSql.sql
+++ AddressBookSql.sql
@@ -3,3 +3,15 @@ Create database AddressBook;
 
 use AddressBook;
 
+--Uc2--
+create table Address_Book_Table
+(
+FirstName varchar(20) not null,
+LastName varchar(20),
+address varchar(100),
+City varchar(20),
+StateName varchar(25),
+ZipCode int,
+Phonenum bigint,
+EmailId varchar(20)
+);
