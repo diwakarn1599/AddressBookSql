1cbf6f3778db95921116fabe7e17a88f0e4d0ddf
Diwakar <diwakar.n@tvsnext.io>
2021-08-01 21:16:16 +0530

    [Diwakar] Insert Data into table

--- AddressBookSql.sql
+++ AddressBookSql.sql
@@ -15,3 +15,12 @@ ZipCode int,
 Phonenum bigint,
 EmailId varchar(20)
 );
+
+--Uc3--
+Insert into Address_Book_Table values('Diwakar','N','Ambattur','chennai','Tn',600062,9876543210,'asdf@gmail.com'),
+('Gayathri','Sri','Egmore','chennai','Tn',600072,9852343210,'qwer@gmail.com'),
+('Surya','S','mogappair','chennai','Tn',600022,9876145210,'surya@gmail.com'),
+('Siva','Karthi','Chik','Manglore','karnataka',56872,9885443210,'qazx@gmail.com');
+
+
+select * from Address_Book_Table;
