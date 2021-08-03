3dbb97dc75da030741264ef829963b89ba64a342
Diwakar <diwakar.n@tvsnext.io>
2021-08-03 16:22:53 +0530

    [Diwakar] Draw er diagram and re do operarions based on that

--- AddressBookSql.sql
+++ AddressBookSql.sql
@@ -76,3 +76,92 @@ Select count(*)as CountAbNames, AdressBookName  from Address_Book_Table group by
 --Uc11--
 Insert into Address_Book_Table values ('Surya','S','mogappair','chennai','Tn',600022,9876145210,'surya@gmail.com','FamilyAddressBook','Family');
 select * from Address_Book_Table;
+
+----------------------------------------------------------
+--Create AddressBook Table
+CREATE TABLE AddressBook(
+	AddressBookId INT IDENTITY(1,1) PRIMARY KEY,
+	AddressBookName VARCHAR(30)
+);
+--Create Persons Table
+CREATE TABLE PersonTypes(
+	PersonTypeId INT IDENTITY(1,1) PRIMARY KEY,
+	PersonType varchar(50),
+	--PersonId INT  FOREIGN KEY REFERENCES Person(PersonId)
+);
+
+
+--Create Table Persons
+CREATE TABLE Person(
+	PersonId INT IDENTITY(1,1) PRIMARY KEY,
+	AddressBookId INT FOREIGN KEY REFERENCES AddressBook(AddressBookId),
+	PersonTypeId INT  FOREIGN KEY REFERENCES PersonTypes(PersonTypeId),
+	FirstName varchar(30),
+	LastName varchar(30),
+	Address varchar(30),
+	City varchar(30),
+	StateName varchar(30),
+	ZipCode int,
+	PhoneNumber bigint,
+	EmailId varchar(30)
+);
+
+
+
+
+
+--------------------------------------------------------
+--Insert Values into address books
+INSERT INTO AddressBook VALUES ('Diwakar'),('Rajini');
+
+SELECT * FROM AddressBook;
+
+--Insert values in persons type
+INSERT INTO PersonTypes VALUES ('Family'),('Friend'),('Profession');
+SELECT * FROM PersonTypes;
+
+
+---Insert Values into Persons
+INSERT INTO Person VALUES (1,1,'Gayathri','Sri','Egmore','Chennai','Tn',600062,9876543210,'asdf@asf.com');
+INSERT INTO Person VALUES (1,2,'Nayantara','N','Palakkad','Kochin','Kerala',602536,9985543210,'qwer@asf.com'),(2,3,'Kamal','Haasan','Adyar','Chennai','Tn',674536,9985595210,'kamal@asf.com');
+INSERT INTO Person VALUES (1,2,'Vijay','N','Palakkad','Kochin','Kerala',602536,9985543210,'qwer@asf.com');
+SELECT * FROM Person;
+
+
+
+--Insert into persons type map tables
+--INSERT INTO PersonTypesMap VALUES (1,1),(2,2),(3,3);
+--SELECT * FROM PersonTypesMap;
+
+-----------------------------------------------------------------------------
+--Retrive All Data---
+SELECT ab.AddressBookId,ab.AddressBookName,p.PersonId,p.FirstName,p.LastName,p.Address,p.City,p.StateName,p.ZipCode,
+p.PhoneNumber,p.EmailId,pt.PersonType,pt.PersonTypeId FROM
+AddressBook AS ab 
+INNER JOIN Person AS p ON ab.AddressBookId = p.AddressBookId
+INNER JOIN PersonTypes AS pt ON pt.PersonTypeId = p.PersonTypeId;
+
+-------------------------------------------------------------------
+--Retrieve based on city and state--
+SELECT ab.AddressBookId,ab.AddressBookName,p.PersonId,p.FirstName,p.LastName,p.Address,p.City,p.StateName,p.ZipCode,
+p.PhoneNumber,p.EmailId,pt.PersonType,pt.PersonTypeId FROM
+AddressBook AS ab 
+INNER JOIN Person AS p ON ab.AddressBookId = p.AddressBookId AND (p.City='Chennai' OR p.StateName='Tn')
+INNER JOIN PersonTypes AS pt ON pt.PersonTypeId = p.PersonTypeId;
+
+-------------------------------------------------------------------------
+--Count based on city---
+Select Count(*) As Count,StateName,City from Person group by StateName,City;
+
+---------------------------------------------------------------------------
+--Sort based on first name
+SELECT ab.AddressBookId,ab.AddressBookName,p.PersonId,p.FirstName,p.LastName,p.Address,p.City,p.StateName,p.ZipCode,
+p.PhoneNumber,p.EmailId,pt.PersonType FROM
+AddressBook AS ab 
+INNER JOIN Person AS p ON ab.AddressBookId = p.AddressBookId
+INNER JOIN PersonTypes AS pt ON pt.PersonTypeId = p.PersonTypeId ORDER BY p.FirstName ;
+------------------------------------------------------------------------------
+--Retreive based on person types---
+SELECT COUNT(p.PersonTypeId) as Count,pt.PersonType FROM
+Person AS p 
+INNER JOIN PersonTypes AS pt ON pt.PersonTypeId = p.PersonTypeId GROUP BY p.PersonTypeId,pt.PersonType;
